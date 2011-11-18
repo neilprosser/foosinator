b61ea6a99d24c2589c1e5d70e53e5511010dc13b
Neil Prosser <neil.prosser@gmail.com>
2011-11-18 22:12:00 +0000

    A first attempt at getting some information out of the service

--- controllers.js
+++ controllers.js
@@ -3,10 +3,10 @@ angular.service("serviceBaseUrl", function() {
 }, { $eager: "true" });
 
 function FoosController($xhr, serviceBaseUrl) {
-    var self = this;
     this.$xhr = $xhr;
     this.serviceBaseUrl = serviceBaseUrl;
     this.results = [];
+    this.players = [];
     this.readResults();
 };
 
@@ -20,10 +20,80 @@ FoosController.prototype = {
 
     callback : function(status, response) {
         this.results = response.results;
+        this.players = this.readPlayers(this.results);
     },
 
     errorCallback : function(status) {
         console.log("Error: " + status);
+    },
+
+    readPlayers : function(results) {
+        var playersSet = {};
+        angular.forEach(results, function(key, value) {
+            this[key.team1.defender] = "";
+            this[key.team1.attacker] = "";
+            this[key.team2.defender] = "";
+            this[key.team2.attacker] = "";
+        }, playersSet);
+        var players = [];
+        angular.forEach(playersSet, function(key, value) {
+            this.push({ name: value });
+        }, players);
+        return angular.Array.orderBy(players, "name");
+    },
+
+    nameIsNotLeft : function(item) {
+        if (!item || !this.left) {
+            return false;
+        }
+        return item.name !== this.left.name;
+    },
+
+    gamesIncludingPlayer : function(player) {
+        if (!player) {
+            return [];
+        }
+
+        var self = this;
+        return angular.Array.filter(this.results, function(game) {
+            return self.teamIncludesPlayer(game.team1, player) || self.teamIncludesPlayer(game.team2, player);
+        });
+    },
+
+    gamesIncludingPlayers : function(playerOne, playerTwo) {
+        if (!playerOne || !playerTwo) {
+            return [];
+        }
+
+        var self = this;
+        return angular.Array.filter(this.results, function(game) {
+            if ((self.teamIncludesPlayer(game.team1, playerOne) || self.teamIncludesPlayer(game.team2, playerOne)) && (self.teamIncludesPlayer(game.team1, playerTwo) || self.teamIncludesPlayer(game.team2, playerTwo))) {
+                return true;
+            }
+            return false;
+        });
+    },
+
+    gamesIncludingPlayersOnSameTeam : function(playerOne, playerTwo) {
+        if (!playerOne || !playerTwo) {
+            return [];
+        }
+
+        var self = this;
+        return angular.Array.filter(this.results, function(game) {
+            if ((self.teamIncludesPlayer(game.team1, playerOne) && self.teamIncludesPlayer(game.team1, playerTwo)) ||
+                    (self.teamIncludesPlayer(game.team2, playerOne) && self.teamIncludesPlayer(game.team2, playerTwo))) {
+                return true;
+            }
+            return false;
+        });
+    },
+
+    teamIncludesPlayer : function(team, player) {
+        if (team.defender === player.name || team.attacker === player.name) {
+            return true;
+        }
+        return false;
     }
 
 };
